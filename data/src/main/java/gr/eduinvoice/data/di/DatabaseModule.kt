3c6c64419760393ed8eaa0f57af635c7818bf430
tsampolion <dim.tsampallas@gmail.com>
2025-07-31 17:27:56 +0300

    fix(data): recover database on corruption

--- data/src/main/java/gr/eduinvoice/data/di/DatabaseModule.kt
+++ data/src/main/java/gr/eduinvoice/data/di/DatabaseModule.kt
@@ -6,10 +6,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import gr.eduinvoice.data.database.DatabaseConstants
 import gr.eduinvoice.data.database.EduInvoiceDatabase
 import gr.eduinvoice.data.user.UserPreferencesRepository
 import net.sqlcipher.database.SQLiteDatabase
 import kotlinx.coroutines.runBlocking
+import android.database.sqlite.SQLiteException
+import android.util.Log
 import javax.inject.Singleton
 
 @Module
@@ -25,7 +28,13 @@ object DatabaseModule {
         val pass = runBlocking { prefs.getDbPassphrase() }
         require(!pass.isNullOrEmpty()) { "Database passphrase unavailable" }
         val passphrase = SQLiteDatabase.getBytes(pass!!.toCharArray())
-        return EduInvoiceDatabase.getDatabase(context, passphrase)
+        return try {
+            EduInvoiceDatabase.getDatabase(context, passphrase)
+        } catch (e: SQLiteException) {
+            Log.e("DatabaseModule", "Database open failed, attempting recovery", e)
+            context.getDatabasePath(DatabaseConstants.DATABASE_NAME).delete()
+            EduInvoiceDatabase.getDatabase(context, passphrase)
+        }
     }
 
     // DatabaseModule only provides the Room database instance.
