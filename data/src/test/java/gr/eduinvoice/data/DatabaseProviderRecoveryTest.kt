abe7da59e9fc50f800a464392a1b9c1de546157a
tsampolion <dim.tsampallas@gmail.com>
2025-08-02 02:09:58 +0300

    fix(database): recover corrupt db on provider

---
+++ data/src/test/java/gr/eduinvoice/data/DatabaseProviderRecoveryTest.kt
@@ -0,0 +1,44 @@
+package gr.eduinvoice.data
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import gr.eduinvoice.data.database.DatabaseConstants
+import gr.eduinvoice.data.di.DatabaseModule
+import gr.eduinvoice.data.user.UserPreferencesRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.junit.Assert.assertTrue
+
+@RunWith(RobolectricTestRunner::class)
+class DatabaseProviderRecoveryTest {
+    private lateinit var context: Context
+    private lateinit var prefs: UserPreferencesRepository
+
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+        prefs = UserPreferencesRepository(context)
+        runBlocking { prefs.setDbPassphrase("secret") }
+    }
+
+    @After
+    fun tearDown() {
+        context.deleteDatabase(DatabaseConstants.DATABASE_NAME)
+    }
+
+    @Test
+    fun recreatesBadDbOnProviderInvocation() = runBlocking {
+        val dbFile = context.getDatabasePath(DatabaseConstants.DATABASE_NAME)
+        dbFile.parentFile?.mkdirs()
+        dbFile.writeBytes(ByteArray(32) { 3 })
+        val originalSize = dbFile.length()
+
+        val db = DatabaseModule.provideEduInvoiceDatabase(context, prefs)
+        assertTrue(dbFile.length() != originalSize)
+        db.close()
+    }
+}
